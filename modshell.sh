03dec98e4979e005cfa8c9a552b59cb91ab02437
Leulz <sir.meira@gmail.com>
2018-11-14 20:30:01 -0200

    Ignore commands

--- modshell.sh
+++ modshell.sh
@@ -4,13 +4,25 @@
 #todo echo PS1 done
 #todo append command to log
 #todo cd case skip
-#todo ignore commands
+#todo ignore commands: done, but what about pipe?
 
 IGNORED_COMMANDS=$(help | awk 'NR > 15 {print $1}')
 
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
 while true
 do
+  echo -n `logname`@`hostname`:`dirs +0`\$" "
 	read CMD
-	echo -n `logname`@`hostname`:`dirs +0`\$" "
-	(time $CMD) 2>>/var/tmp/log
+  MAIN_COMMAND=$(echo $CMD | awk '{print $1}')
+  if containsElement $MAIN_COMMAND ${IGNORED_COMMANDS[@]}; then
+    $CMD
+  else
+    (time $CMD) 2>>/var/tmp/log
+  fi
 done
