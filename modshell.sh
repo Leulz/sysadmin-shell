3e056a67f0e0da7e5b8ffcaffc3b0dad6ac874f9
Leulz <sir.meira@gmail.com>
2018-11-15 15:58:33 -0200

    Adding history functionality

--- modshell.sh
+++ modshell.sh
@@ -3,7 +3,6 @@
 #todo append command to log: done, but where should the log be? Should it have the user's name?
 #todo empty command is being timed, probably should not be
 #How to deal with piped commands that have both ignored and non-ignored commands?
-#todo get bash history, see https://unix.stackexchange.com/questions/52091/read-with-history
 
 # The following may be useful:
 # TIMEFORMAT="$CMD"$'\t%E real,\t%U user,\t%S sys'
@@ -15,6 +14,7 @@
 #Is there a better way? Also, we should load .profile too.
 
 IGNORED_COMMANDS=$(help | awk 'NR > 15 {print $1}')
+history -c -r "$HOME/.modshell_history"
 
 containsElement () {
   local e match="$1"
@@ -28,12 +28,15 @@ trap 'echo "Command ignored, press <<Enter>> to continue.";continue' 2
 while true
 do
 	read -e -p "`logname`@`hostname`:`dirs +0`\$ " CMD
+
   #If CMD is ^D, read will have exit code "1"
   if [[ $? == 1 ]]; then 
     trap 2
     echo ""
-    exit
+    break
   fi
+
+ history -s "$CMD"
 
   MAIN_COMMAND=$(echo $CMD | awk '{print $1}')
   if containsElement $MAIN_COMMAND ${IGNORED_COMMANDS[@]}; then
@@ -42,3 +45,5 @@ do
     /usr/bin/time -f "$CMD\t%e real,\t%U user,\t%S sys" -ao /var/tmp/log bash -ci "$CMD"
   fi
 done
+
+history -a "$HOME/.modshell_history"
