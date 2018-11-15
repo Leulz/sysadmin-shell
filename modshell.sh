44808c3563cc2ea815db1e33e7677d6fa12c13a3
Leulz <sir.meira@gmail.com>
2018-11-15 12:37:19 -0200

    Making the script load .bashrc

--- modshell.sh
+++ modshell.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#!/bin/bash -i
 
 #todo append command to log: done, but where should the log be? Should it have the user's name?
 #todo empty command is being timed, probably should not be
 #How to deal with piped commands that have both ignored and non-ignored commands?
 #todo get bash history, see https://unix.stackexchange.com/questions/52091/read-with-history
+
+# The following may be useful:
+# TIMEFORMAT="$CMD"$'\t%E real,\t%U user,\t%S sys'
+# exec 3>&1 4>&2
+# { time $CMD 1>&3 2>&4; } >/var/tmp/log 2>&1 
+# exec 3>&- 4>&-
+
+#Currently, when timing the command, we use the flags -c and -i. -i forces .bashrc to be loaded. 
+#Is there a better way? Also, we should load .profile too.
 
 IGNORED_COMMANDS=$(help | awk 'NR > 15 {print $1}')
 
@@ -30,6 +39,6 @@ do
   if containsElement $MAIN_COMMAND ${IGNORED_COMMANDS[@]}; then
     $CMD
   else
-    /usr/bin/time -f "$CMD\t%e real,\t%U user,\t%S sys" -ao /var/tmp/log bash -c "$CMD"
+    /usr/bin/time -f "$CMD\t%e real,\t%U user,\t%S sys" -ao /var/tmp/log bash -ci "$CMD"
   fi
 done
