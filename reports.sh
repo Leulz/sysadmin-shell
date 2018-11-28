c625b549fb3a8dda3c2e604754e5b9eb8998c69d
Anderson Menezes <anderson.menezes@ccc.ufcg.edu.br>
2018-11-28 00:50:05 -0300

    histogram

--- reports.sh
+++ reports.sh
@@ -3,10 +3,12 @@
 # month format: 11/2018
 
 if [[ -z $1 ]]; then 
-  echo "Should pass month"
+  echo "Should pass month. Ex: reports.sh 11/2018"
   exit
 fi
 
 MONTH=$1
 
 grep $MONTH /var/tmp/log | awk -F "\t" -v usrTime=0 -v sysTime=0 '{split($(NF-1),sys," "); sysTime+=sys[1]; split($(NF-2),usr," "); usrTime+=usr[1];} END {print "total user: ", usrTime; print "total sys: ", sysTime}'
+
+grep $MONTH /var/tmp/log | awk -F\| '{for(i=1;i<=NF;i++) print $i}' | awk '{print $1}' | sort | uniq -c
